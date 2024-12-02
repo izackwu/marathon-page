8b8219d5de21df9f9b1ca87be6fe77df919018f2
Zack Wu <hi@zackwu.com>
2024-12-02 14:54:12 +0800

    misc: set node version and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  define: { 
+  define: {
     'import.meta.env.VITE_SITE_TITLE': '"My Marathon Journey"'
   },
 });
