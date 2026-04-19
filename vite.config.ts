74187637530e78700d83e00b126d94a0f9c5491b
Zack Wu <hi@zackwu.com>
2026-04-19 14:50:21 +0800

    feat: update favicon and rename site (#12)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: {
-    "import.meta.env.VITE_SITE_TITLE": '"My Marathon Journey"',
+    "import.meta.env.VITE_SITE_TITLE": '"Zack\'s Marathon Journey"',
   },
 });
